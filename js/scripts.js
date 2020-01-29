cf0423dedd8c7d530c79144fc6d1c7df880222a0
Alex Skreen and Erich Richter <example@example.com>
2020-01-29 13:42:56 -0800

    add computer dice roll button. value and turnTotal both function properly

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ var dice = {
 // var result = parseInt(dice.roll());
 turnTotal = 0;
 player1Total = 0;
-
+computerTotal = 0;
 
 
 var rollResults = function (result) {
@@ -16,6 +16,7 @@ var rollResults = function (result) {
 
     if (result === 1) {
         alert("You busted! That is the end of your turn")
+        alert("Computer's turn! Click 'computer roll' to start their turn")
         turnTotal = 0;
     }
     else if (result === 2 || result === 3 || result === 4 || result === 5 || result === 6) {
@@ -36,6 +37,41 @@ else {
 }
 };
 
+var computerRoll = function(result){
+    var result = dice.roll();
+
+    if (result === 1) {
+        alert("You busted! That is the end of your turn")
+        alert("Computer's turn! Click 'computer roll' to start their turn")
+        turnTotal = 0;
+    }
+    else if (result === 2 || result === 3 || result === 4 || result === 5 || result === 6) {
+        turnTotal += result;
+    }
+}
+//     var result = dice.roll();
+//     if (result === 1) {
+//         alert("Computer busted! That is the end of their turn")
+//         alert("Player 1's turn! Click'Roll' to start your turn!")
+//         turnTotal = 0;
+//     }
+//     else if (result === 2 || result === 3 || result === 4 || result === 5 || result === 6) {
+//         turnTotal += result;
+//         dice.roll();
+//         console.log("made it to 2nd dice roll!")
+//             if (result === 1) {
+//                 alert("Computer busted! That is the end of their turn")
+//                 alert("Player 1's turn! Click'Roll' to start your turn!")
+//                 turnTotal = 0;
+//             }
+//             else if (result === 2 || result === 3 || result === 4 || result === 5 || result === 6) {
+//                 turnTotal += result;
+//                 computerTotal = computerTotal + turnTotal;
+//                 turnTotal = 0;
+
+//     }
+// }
+// }
 
 
 $(document).ready(function () {
@@ -51,7 +87,21 @@ $(document).ready(function () {
         addPlayer1Score();
         console.log(player1Total);
     });
+    $("#buttonComputer").click(function (event) {
+        event.preventDefault();
+        console.log("button works!")
+        computerRoll();
+        console.log(turnTotal)
 
+    });
+    //     $("#buttonComputer").click(function (event) {
+    //     event.preventDefault();
+    //     console.log("Button works!")
+    //     // computerRoll();
+    //     // console.log(turnTotal);
+    //     // console.log(computerTotal);
+    //     // console.log(turnTotal);
+    // });
 
 
 
