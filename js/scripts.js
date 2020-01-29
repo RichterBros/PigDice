1b500d5c80892c5cdc347e4c02f8ef41d14359cd
Alex Skreen and Erich Richter <example@example.com>
2020-01-29 10:49:58 -0800

    rollResults function works properly. turnTotal adds and zeros out propersly

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,23 @@ var dice = {
         return randomNumber;
     }
 }
-var result = parseInt(dice.roll());
-//console.log(result);
+// var result = parseInt(dice.roll());
 turnTotal = 0;
 player1Total = 0;
 
 
+
 var rollResults = function(result){
+    var result = dice.roll();
+    console.log(result)
 
 if (result === 1){
     alert("You busted! That is the end of your turn")
-    //player1Total = player1Total + turnTotal
+    turnTotal = 0;
 }
 else if (result === 2 || result === 3 || result === 4  || result === 5 || result === 6 ) {
     turnTotal  += result;
+
 }
 
 }
@@ -27,9 +30,9 @@ else if (result === 2 || result === 3 || result === 4  || result === 5 || result
 $(document).ready(function () {
     $("#button").click(function (event) {
         event.preventDefault();
-        rollResults(result);
-        console.log(result);
+        rollResults();
         console.log(turnTotal);
+        // console.log(turnTotal);
 
         
     });
