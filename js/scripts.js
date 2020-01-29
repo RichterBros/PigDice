97a6d368814ac962b67d353086be807202dc568c
Alex Skreen and Erich Richter <example@example.com>
2020-01-29 11:24:46 -0800

    add hold button. functions properly for player 1

--- js/scripts.js
+++ js/scripts.js
@@ -11,30 +11,37 @@ player1Total = 0;
 
 
 
-var rollResults = function(result){
+var rollResults = function (result) {
     var result = dice.roll();
-    console.log(result)
 
-if (result === 1){
-    alert("You busted! That is the end of your turn")
-    turnTotal = 0;
+    if (result === 1) {
+        alert("You busted! That is the end of your turn")
+        turnTotal = 0;
+    }
+    else if (result === 2 || result === 3 || result === 4 || result === 5 || result === 6) {
+        turnTotal += result;
+    }
 }
-else if (result === 2 || result === 3 || result === 4  || result === 5 || result === 6 ) {
-    turnTotal  += result;
 
+var addPlayer1Score = function (){
+    player1Total = player1Total + turnTotal
+    turnTotal =0;
 }
 
-}
 
 
 $(document).ready(function () {
-    $("#button").click(function (event) {
+    $("#buttonRoll").click(function (event) {
         event.preventDefault();
         rollResults();
         console.log(turnTotal);
         // console.log(turnTotal);
+    });
 
-        
+        $("#buttonHold").click(function (event) {
+        event.preventDefault();
+        addPlayer1Score();
+        console.log(player1Total);
     });
 
 
